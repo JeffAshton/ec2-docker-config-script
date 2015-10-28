e985ddb72f58f01d0a8622e3894eb5db1836f2e2
Jeff Ashton <jeff.ashton@cantire.com>
2015-10-28 11:01:27 -0400

    Adding setup script

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ -z "$DOCKER_COMPOSE_VERSION" ]; then
+  DOCKER_COMPOSE_VERSION=1.4.2
+fi
+
+apt-get update -y
+
+# docker
+curl -sSL https://get.docker.com | sh
+
+# docker-compose
+curl -L https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
