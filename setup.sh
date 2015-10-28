a004815339825fb1e7820eb53291ef078e567941
Jeff Ashton <jeff.ashton@cantire.com>
2015-10-28 18:26:53 -0400

    updating script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ -z "$EC2_USERNAME" ]; then
+  EC2_USERNAME=ubuntu
+fi
+
 if [ -z "$DOCKER_COMPOSE_VERSION" ]; then
   DOCKER_COMPOSE_VERSION=1.4.2
 fi
@@ -12,4 +16,6 @@ curl -sSL https://get.docker.com | sh
 # docker-compose
 curl -L https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+
+sudo usermod -aG docker "$EC2_USERNAME"
 
